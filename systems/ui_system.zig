01d1a817ee86c0c5cca6c347992d40b974b64bdf
Michael Scherbakow <ryupold@gmail.com>
2022-05-31 22:50:31 +0200

    cleanup

--- systems/ui_system.zig
+++ systems/ui_system.zig
@@ -3,9 +3,6 @@ const r = @import("../raylib/raylib.zig");
 const log = @import("../log.zig");
 const _ecs = @import("../ecs/ecs.zig");
 const ECS = _ecs.ECS;
-const ray = @cImport({
-    @cInclude("raylib_marshall.h");
-});
 
 pub const UiSystem = struct {
     ecs: *ECS,
@@ -18,12 +15,5 @@ pub const UiSystem = struct {
 
     pub fn deinit(_: *@This()) void {}
 
-    pub fn update(_: *@This(), _: f32) !void {
-        // if (r.GuiButton(
-        //     ray.Rectangle{ .x = 100, .y = 100, .width = 100, .height = 50 },
-        //     "this is a test",
-        // )) {
-        //     log.debug("pressed the button", .{});
-        // }
-    }
+    pub fn ui(_: *@This(), _: f32) !void {}
 };
